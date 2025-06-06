a0b6232fa6830ca1045aa027d07b56b6ee269e9d
ethanchac <ethancha1011@gmail.com>
2025-06-06 18:38:08 -0400

    added the header for future use

---
+++ Yap/frontend/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function Header(){
+    return(
+        <>
+            {/* The logo could be here  and the */}
+
+            {/* future add --> profile picture which redirect*/}
+            {/* Add profile photo here (not added yet)
+            <Link to="/profile">Profile</Link>
+            */}
+        </>
+    )
+}
+
+export default Header;
