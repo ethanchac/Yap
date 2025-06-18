afa0cf6db2f6758fbef7eb142994987bf77db727
ethanchac <ethancha1011@gmail.com>
2025-06-18 19:27:59 -0400

    some simple additions

--- Yap/frontend/components/pages/Settings.jsx
+++ Yap/frontend/components/pages/Settings.jsx
@@ -1,5 +1,13 @@
-function Settings(){
+import Sidebar from "../sidebar/Sidebar";
+import Header from "../header/Header";
 
+function Settings(){
+    return(
+        <>
+            <Sidebar />
+            <Header />
+        </>
+    )
 }
 
 export default Settings;
