4a33c62f257be7f60e5399b300c8b38ebf6eb5e7
ethanchac <ethancha1011@gmail.com>
2025-08-03 18:08:00 -0400

    test

--- Yap/backend/wsgi.py
+++ Yap/backend/wsgi.py
@@ -1,7 +1,12 @@
 from app import app, socketio
 import os
 
-application = socketio
+app = socketio  
+application = socketio  
+
+@app.route('/test')
+def test():
+    return {'message': 'WSGI is working!', 'server': 'gunicorn'}
 
 if __name__ == "__main__":
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
